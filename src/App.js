af30f730f6de972d0e9dcabec344dfe26b54b4e1
Peter <thong01677@gmail.com>
2021-07-28 00:12:40 -0400

    adding the sketch of navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-//Router
+//Routing
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //Styles
+
+import { GlobalStyle } from "./GlobalStyle";
 
 //Component
 import NavigationBar from "./components/NavigationBar";
@@ -13,10 +15,10 @@ import Blog from "./components/Blog";
 const App = () => (
   <Router>
     <NavigationBar />
-    <Routes>
-      <Route path="/" element={<Blogs />} />
-      <Route path="/:blogId" element={<Blog />} />
-    </Routes>
+    {/* //     <Route path="/" element={<Blogs />} />
+  //     <Route path="/:blogId" element={<Blog />} />
+  //   </Routes> */}
+    <GlobalStyle />
   </Router>
 );
 
