51d21904478de732602dfee9ae5da2a999719237
Peter <thong01677@gmail.com>
2021-07-27 22:03:04 -0400

    set up the project(initial commit)

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Blog project</header>
+    </div>
+  );
+}
+
+export default App;
